d39dac4bc88bd8efb0530b80808b9c88f4ab17ab
stephanieEt <stetourneau@laposte.net>
2023-08-31 15:14:43 +0200

    initialisation du projet

---
+++ script.js
@@ -0,0 +1,6 @@
+const clicImage = document.getElementById("montagne1");
+const response = document.getElementById("montagne");
+
+clicImage.addEventListener("click", () => {
+  response.style.background = "green";
+});
